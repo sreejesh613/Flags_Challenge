283189418e79d9f56b1e656ddb87050e37f643be
sreejesh <sreejeshk007@gmail.com>
2025-05-20 15:28:07 +0530

    json model and viewmodel added

---
+++ Flags Challenge/Model/FlagModel.swift
@@ -0,0 +1,26 @@
+//
+//  FlagModel.swift
+//  Flags Challenge
+//
+//  Created by Sreejesh Krishnan on 16/05/25.
+//
+
+import Foundation
+
+struct Questions: Decodable {
+    let questions: [Answer]?
+    init(questions: [Answer]? = nil) {
+        self.questions = questions
+    }
+}
+
+struct Answer: Decodable {
+    let answer_id: Int
+    let countries: [Country]
+    let country_code: String
+}
+
+struct Country: Decodable {
+    let id: Int
+    let country_name: String
+}
---
+++ Flags Challenge/View Model/CountriesViewModel.swift
@@ -0,0 +1,37 @@
+//
+//  CountriesViewModel.swift
+//  Flags Challenge
+//
+//  Created by Sreejesh Krishnan on 20/05/25.
+//
+
+import Foundation
+
+class CountriesViewModel: ObservableObject {
+    @Published var countries: Questions = Questions()
+    @Published var currentCountryIndex = 0
+    
+    var currentAnswer: Answer? {
+        guard ((countries.questions?.indices.contains(currentCountryIndex)) != nil) else { return nil }
+        return countries.questions?[currentCountryIndex]
+    }
+    
+    func loadQuestions() {
+        countries = JsonLoader.loadCountries()!
+        currentCountryIndex = 0
+    }
+    
+    func nextAnswer() {
+        if currentCountryIndex < (countries.questions?.count ?? 0) - 1 {
+            currentCountryIndex += 1
+        } else {
+            currentCountryIndex = 0
+        }
+    }
+    
+    func checkAnswer(selectedAnswer: Int) -> Answer? {
+        countries.questions!.first(where: { question in
+            question.countries.contains(where: { $0.id == selectedAnswer })
+        })
+    }
+}
